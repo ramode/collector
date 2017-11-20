ee51b69542d22095cd0a02bde71cf67e226a2c57
eri <alex-eri@ya.ru>
2017-11-20 12:32:06 +0300

    tsrange

--- collector.c
+++ collector.c
@@ -3,42 +3,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <endian.h>
 
 #include <uv.h>
 
 #include <libpq-fe.h>
 
-#define DEBUG 1
+#define DEBUG
 #define NETFLOW5 5
-#define BUFFER_SIZE 4
+#define BUFFER_SIZE 1
 
 
-static char COPY_HEAD[19] = {0x50, 0x47, 0x43, 0x4f, 0x50, 0x59, 0x0a, 0xff, 0x0d, 0x0a, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-static char COPY_ROW[146] = {
- 0x00, 0x11,
- 0x00, 0x00, 0x00, 0x02, 0x00, 0x00,
- 0x00, 0x00, 0x00, 0x02, 0x00, 0x00,
- 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00,
- 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00,
- 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00,
- 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00,
- 0x00, 0x00, 0x00, 0x02, 0x00, 0x00,
- 0x00, 0x00, 0x00, 0x02, 0x00, 0x00,
- 0x00, 0x00, 0x00, 0x02, 0x00, 0x00,
- 0x00, 0x00, 0x00, 0x02, 0x00, 0x00,
- 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00,
- 0x00, 0x00, 0x00, 0x08, 0x02, 0x20, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00,
- 0x00, 0x00, 0x00, 0x08, 0x02, 0x20, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00,
- 0x00, 0x00, 0x00, 0x08, 0x02, 0x20, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00,
- 0x00, 0x00, 0x00, 0x08, 0x02, 0x20, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00,
- 0x00, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
- 0x00, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-};
-
-static char COPY_IP4[8] = {0, 0, 0, 0x08, 0x02, 0x20, 0, 0x04 };
-static char COPY_NULL[4] = {0xff, 0xff, 0xff, 0xff};
-static char COPY_END[2] = {0xff, 0xff};
-
+#pragma pack(push,1)
 typedef struct {
 	char srcaddr[4];
 	char dstaddr[4];
@@ -73,8 +49,8 @@ typedef struct {
 	uint16_t output;
 	uint32_t pkts;
 	uint32_t octets;
-	uint32_t first;
-	uint32_t last;
+	uint64_t first;
+	uint64_t last;
 	uint16_t sport;
 	uint16_t dport;
 	uint8_t prot;
@@ -95,10 +71,10 @@ uint32_t p3;
 	uint32_t pkts;
 uint32_t p4;
 	uint32_t octets;
-uint32_t p5;
-	uint32_t first;
-uint32_t p6;
-	uint32_t last;
+char p5[9];
+	uint64_t first;
+char p6[4];
+	uint64_t last;
 uint32_t p7;
 	uint16_t sport;
 uint32_t p8;
@@ -135,6 +111,41 @@ uint32_t p16;
 uint32_t p17;
 	mac_t dstmac;
 } pgrow;
+
+#pragma pack(pop)
+
+
+static char COPY_HEAD[19] = {0x50, 0x47, 0x43, 0x4f, 0x50, 0x59, 0x0a, 0xff, 0x0d, 0x0a, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+static char COPY_ROW[sizeof(pgrow)+2] = {
+ 0x00, 0x10,
+ 0x00, 0x00, 0x00, 0x02, 0x00, 0x00,
+ 0x00, 0x00, 0x00, 0x02, 0x00, 0x00,
+ 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00,
+ 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00,
+
+ 0x00, 0x00, 0x00, 0x19, 0x06,
+ 0x00, 0x00, 0x00, 0x08,
+ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+ 0x00, 0x00, 0x00, 0x08,
+ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+
+ 0x00, 0x00, 0x00, 0x02, 0x00, 0x00,
+ 0x00, 0x00, 0x00, 0x02, 0x00, 0x00,
+ 0x00, 0x00, 0x00, 0x02, 0x00, 0x00,
+ 0x00, 0x00, 0x00, 0x02, 0x00, 0x00,
+ 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00,
+ 0x00, 0x00, 0x00, 0x08, 0x02, 0x20, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00,
+ 0x00, 0x00, 0x00, 0x08, 0x02, 0x20, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00,
+ 0x00, 0x00, 0x00, 0x08, 0x02, 0x20, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00,
+ 0x00, 0x00, 0x00, 0x08, 0x02, 0x20, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00,
+ 0x00, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+ 0x00, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+
+static char COPY_IP4[8] = {0, 0, 0, 0x08, 0x02, 0x20, 0, 0x04 };
+static char COPY_NULL[4] = {0xff, 0xff, 0xff, 0xff};
+static char COPY_END[2] = {0xff, 0xff};
+
 
 flow_uint_t *flow_buffer;
 uint16_t flow_in;
@@ -198,32 +209,36 @@ void hexDump (char *desc, void *addr, int len) {
 static void
 exit_nicely(PGconn *conn)
 {
-    PQfinish(conn);
-    exit(1);
+	PQfinish(conn);
+	exit(1);
 }
 
 
 void store() {
-	PGresult   *res;
-
-	uint16_t size = 146*flow_in+19+2;
+	PGresult *res;
+	uint32_t size = 159*flow_in+19+2;
 
 	//uv_buf_t buf;
 	//buf = uv_buf_init(malloc(size), size);
 	//memcpy ( buf.base, &flow_buffer, size);
 
-	char *pgbuffer= (char*) malloc(size);
-	memset(pgbuffer,0,size);
+	char *pgbuffer;
+	pgbuffer = malloc(size);
+
+	printf("buffer %d, size %d, count%d", pgbuffer,size,flow_in);
+
 	char *p;
-	p=pgbuffer;
+	p = pgbuffer;
 	memcpy( pgbuffer,&COPY_HEAD,19);
-	p+=19;
+	p += 19;
 
 	for (uint16_t i=0; flow_in > i ;i++) {
-		memcpy(p,&COPY_ROW,146);
+		printf("%d - %d\n", i, sizeof(COPY_ROW));
+		memcpy(p,&COPY_ROW,sizeof(COPY_ROW));
+		p+=2;
+		pgrow * row = (pgrow*)p;
+		printf("row %d\n",row);
 
-		pgrow *row;
-		row=(pgrow *) p+2;
 
 		row->input = flow_buffer[i].input;
 		row->output = flow_buffer[i].output;
@@ -243,80 +258,76 @@ void store() {
 
 		row->srcmac = flow_buffer[i].srcmac;
 		row->dstmac = flow_buffer[i].dstmac;
-		/*
-		memcpy(p+6, &flow_buffer[i].input, 2);
-		memcpy(p+12, &flow_buffer[i].output, 2);
-		memcpy(p+18, &flow_buffer[i].pkts, 4);
-		memcpy(p+26, &flow_buffer[i].octets, 4);
-		memcpy(p+34, &flow_buffer[i].first, 4);
-		memcpy(p+42, &flow_buffer[i].last, 4);
-		*/
-		//hexDump("addr",&flow_buffer[i].srcaddr,4);
 
-		p+=146;
+		hexDump("addr",row,157);
+
+		p += 157;
+		row = NULL;
 	}
-
 	flow_in=0;
 	memcpy(p, &COPY_END, 2);
+	p = NULL;
 
-	hexDump("f",pgbuffer,size);
-
+	hexDump("f", pgbuffer, size);
 	res = PQexec(postgres, "COPY collector FROM STDIN WITH (FORMAT binary)");
 	if (PQresultStatus(res) == PGRES_COPY_IN ) {
-
-		PQputCopyData(postgres,pgbuffer,size);
-		PQputCopyEnd(postgres,NULL);
+		PQputCopyData(postgres, pgbuffer, size);
+		PQputCopyEnd(postgres, NULL);
 	};
 	PQendcopy(postgres);
-
+	free(pgbuffer);
 }
 
 
 
 void alloc_cb(uv_handle_t *handle, size_t size, uv_buf_t *buf)
 {
-    *buf = uv_buf_init(malloc(size), size);
+	*buf = uv_buf_init(malloc(size), size);
 }
 
 
 #define nf5header_size 24
+#pragma pack(push,1)
 
 struct nf5header {
-  uint16_t version;
-  uint16_t count;
-  uint32_t uptime;
-  uint32_t timestamp;
-  uint32_t nanosecs;
-  uint32_t sequence;
+	uint16_t version;
+	uint16_t count;
+	uint32_t uptime;
+	uint32_t timestamp;
+	uint32_t nanosecs;
+	uint32_t sequence;
 };
 
 #define nf5data_size 48
 
+
 struct nf5data {
-  char srcaddr[4];
-  uint32_t dstaddr;
-  uint32_t nexthop;
-  uint16_t input;
-  uint16_t output;
-  uint32_t dPkts;
-  uint32_t dOctets;
-  uint32_t first;
-  uint32_t last;
-  uint16_t srcport;
-  uint16_t dstport;
+	char srcaddr[4];
+	uint32_t dstaddr;
+	uint32_t nexthop;
+	uint16_t input;
+	uint16_t output;
+	uint32_t dPkts;
+	uint32_t dOctets;
+	uint32_t first;
+	uint32_t last;
+	uint16_t srcport;
+	uint16_t dstport;
 
-  uint8_t pad1;
+	uint8_t pad1;
 
-  uint8_t tcp_flags;
-  uint8_t prot;
-  uint8_t tos;
-  uint16_t src_as;
-  uint16_t dst_as;
-  uint8_t src_mask;
-  uint8_t dst_mask;
+	uint8_t tcp_flags;
+	uint8_t prot;
+	uint8_t tos;
+	uint16_t src_as;
+	uint16_t dst_as;
+	uint8_t src_mask;
+	uint8_t dst_mask;
 
-  uint16_t pad2;
+	uint16_t pad2;
 };
+
+#pragma pack(pop)
 
 void parse_five(const struct uv_buf_t *buf, const struct sockaddr *addr) {
 
@@ -324,83 +335,62 @@ void parse_five(const struct uv_buf_t *buf, const struct sockaddr *addr) {
 //	memcpy(head, buf->base,sizeof(nf5header));
 
 	struct nf5header *nhead = (struct nf5header *) buf->base;
-	uint32_t delta = ntohl(nhead->timestamp)*1000 + ntohl(nhead->nanosecs)/1000000 - ntohl(nhead->uptime);
+	uint64_t delta = ((uint64_t) ntohl(nhead->timestamp) - 946684800)*1000000
+		+ ntohl(nhead->nanosecs)/1000
+		- ((uint64_t) ntohl(nhead->uptime))*1000;
 
-	fprintf(stderr, "%d/n",delta);
+	fprintf(stderr, "%d\n",delta);
 
 	for (uint16_t i=0; i<ntohs(nhead->count); i++) {
 		struct nf5data *ndata = (struct nf5data *) (buf->base+nf5header_size+nf5data_size*i);
-
 		struct sockaddr_in * sensor = (struct sockaddr_in*) addr;
-
-		hexDump("addr",&ndata->srcaddr,4);
-
-		uint32_t first = htonl((ntohl(ndata->first)+delta)/1000);
-		uint32_t last = htonl((ntohl(ndata->last)+delta)/1000);
+		uint64_t first = htobe64( ( (uint64_t) ntohl(ndata->first)*1000+delta));
+		uint64_t last = htobe64(( (uint64_t) ntohl(ndata->last)*1000+delta));
 		uint32_t sequence = htonl(ntohl(nhead->sequence)+i);
-
-
 		flow_uint_t flow;
-
-	 memcpy(&flow.srcaddr,&ndata->srcaddr,4);
-	 memcpy(&flow.dstaddr,&ndata->dstaddr,4);
-	 memcpy(&flow.nexthop,&ndata->nexthop,4);
-	 memcpy(&flow.input,&ndata->input,2);
-	 memcpy(&flow.output,&ndata->output,2);
-	 memcpy(&flow.pkts,&ndata->dPkts,4);
-	 memcpy(&flow.octets,&ndata->dOctets,4);
-	 memcpy(&flow.first,&first,4);
-	 memcpy(&flow.last, &last, 4);
-	 memcpy(&flow.sport,&ndata->srcport,2);
-	 memcpy(&flow.dport,&ndata->dstport,2);
-	 memcpy(&flow.prot,&ndata->prot,1);
-	 memcpy(&flow.sensor,&sensor->sin_addr.s_addr,4);
-	 memcpy(&flow.sensor_port,&sensor->sin_port,2);
-	 memcpy(&flow.sequence, &sequence, 4);
-
-	 memset(&flow.srcmac,0,6);
-	 memset(&flow.dstmac,0,6);
-
-	flow_buffer[flow_in] = flow;
-	hexDump("addr", &flow.srcaddr,4);
-	if (++flow_in == BUFFER_SIZE)
-		store();
-
-
+		memcpy(&flow.srcaddr,&ndata->srcaddr,4);
+		memcpy(&flow.dstaddr,&ndata->dstaddr,4);
+		memcpy(&flow.nexthop,&ndata->nexthop,4);
+		memcpy(&flow.input,&ndata->input,2);
+		memcpy(&flow.output,&ndata->output,2);
+		memcpy(&flow.pkts,&ndata->dPkts,4);
+		memcpy(&flow.octets,&ndata->dOctets,4);
+		memcpy(&flow.first,&first,8);
+		memcpy(&flow.last, &last, 8);
+		memcpy(&flow.sport,&ndata->srcport,2);
+		memcpy(&flow.dport,&ndata->dstport,2);
+		memcpy(&flow.prot,&ndata->prot,1);
+		memcpy(&flow.sensor,&sensor->sin_addr.s_addr,4);
+		memcpy(&flow.sensor_port,&sensor->sin_port,2);
+		memcpy(&flow.sequence, &sequence, 4);
+		memset(&flow.srcmac,0,6);
+		memset(&flow.dstmac,0,6);
+		flow_buffer[flow_in] = flow;
+		if (++flow_in == BUFFER_SIZE)	store();
 	};
-
 }
 
 
 
 void recv_cb(struct uv_udp_s *handle, long int nread, const struct uv_buf_t *buf, const struct sockaddr *addr, unsigned int flags)
 {
-    if (nread <= 0) {
-      free(buf->base);
-      return;
-    };
+	if (nread <= 0) {
+		free(buf->base);
+		return;
+	};
 
 #ifdef DEBUG
-    char sender[17] = { 0 };
-    uv_ip4_name((struct sockaddr_in*) addr, sender, 16);
-    fprintf(stderr, "Recv from %s ", sender);
+	char sender[17] = { 0 };
+	uv_ip4_name((struct sockaddr_in*) addr, sender, 16);
 #endif
-
-    uint16_t *as_s = (uint16_t*)buf->base;
-    uint16_t version =  ntohs( as_s[0]);
-
-
-    switch(version) {
-      case NETFLOW5 :
-       parse_five(buf,addr);
-       break;
-    };
-
-    free(buf->base);
-
-//    uv_udp_send_t *req = malloc(sizeof(*req));
-//    uv_buf_t ans = uv_buf_init("Hello world!", 13);
-//    uv_udp_send(req, handle, &ans, 1, addr, send_cb);
+	uint16_t *as_s = (uint16_t*)buf->base;
+	uint16_t version =  ntohs( as_s[0]);
+	switch(version) {
+		case NETFLOW5 :
+			parse_five(buf,addr);
+			break;
+	};
+	free(buf->base);
 }
 
 int postgres_connect() {
@@ -418,12 +408,9 @@ int postgres_connect() {
 
 int main(int argc, char **argv)
 {
-
 	while(postgres_connect()>0) sleep(1) ;
-
 	flow_buffer = malloc(sizeof(flow_t)*BUFFER_SIZE);
 	flow_in=0;
-	fprintf(stderr, "Binding\n");
 	uv_udp_t socket;
 	struct sockaddr_in addr;
 	uv_udp_init(uv_default_loop(), &socket);
--- collector.sql
+++ collector.sql
@@ -1,19 +1,22 @@
 CREATE UNLOGGED TABLE if not exists collector (
+	"input" 	smallint NOT NULL,	--uint16_t input;
+	"output"	smallint NOT NULL,	--uint16_t output;
+	"pkts"  	int NOT NULL,	--uint32_t pkts;
+	"octets"	int NOT NULL,	--uint32_t octets;
+	"time"  	tsrange NOT NULL,	--uint32_t first;
+	"sport" 	smallint NOT NULL,	--uint16_t sport;
+	"dport" 	smallint NOT NULL,	--uint16_t dport;
+	"prot"  	smallint NOT NULL,	--uint8_t prot;
+	"sensor_port"	smallint NOT NULL,	--uint16_t sensor_port;
+	"sequence"	int NOT NULL,	--uint32_t sequence;
+	"sensor"	inet NOT NULL,	--uint32_t sensor;
 	"srcaddr"	inet NOT NULL,	--uint32_t srcaddr;
 	"dstaddr"	inet NOT NULL,	--uint32_t dstaddr;
 	"nexthop"	inet NOT NULL,	--uint32_t nexthop;
-	"input" 	smallserial NOT NULL,	--uint16_t input;
-	"output"	smallserial NOT NULL,	--uint16_t output;
-	"pkts"  	serial NOT NULL,	--uint32_t pkts;
-	"octets"	serial NOT NULL,	--uint32_t octets;
-	"first" 	serial NOT NULL,	--uint32_t first;
-	"last"  	serial NOT NULL,	--uint32_t last;
-	"sport" 	smallserial NOT NULL,	--uint16_t sport;
-	"dport" 	smallserial NOT NULL,	--uint16_t dport;
-	"prot"  	smallserial NOT NULL,	--uint8_t prot;
-	"sensor"	inet NOT NULL,	--uint32_t sensor;
-	"sensor_port"	smallserial NOT NULL,	--uint16_t sensor_port;
-	"sequence"	serial NOT NULL,	--uint32_t sequence;
-	"srcmac"	macaddr NULL,	--char[6] srcmac;
-	"dstmac"	macaddr NULL	--char[6] dstmac;
-) WITH (OIDS=FALSE, fillfactor=100);
+	"srcmac"	macaddr NOT NULL,	--char[6] srcmac;
+	"dstmac"	macaddr NOT NULL	--char[6] dstmac;
+) WITH (
+	OIDS=FALSE,
+	fillfactor=100,
+	autovacuum_enabled = false
+);
